038185220f4edf7a9d04cf55c6c7560ec680a44d
bhumikatiwari16 <bhumikatiwari1603@gmail.com>
2019-11-16 22:55:00 -0800

    Post Controller

--- src/main/java/Community/controller/PostController.java
+++ src/main/java/Community/controller/PostController.java
@@ -1,13 +1,17 @@
 package Community.controller;
 
 import java.util.List;
+import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import Community.model.Post;
@@ -34,4 +38,20 @@ public class PostController {
 	public List<Post> getAll() {
 		return postService.getAll();
 	}
+	
+	@RequestMapping(path = "/posts/filterByCategory")
+	public List<Post> findByCategory(String category) {
+		return postService.findByCategory(category);
+	}
+	
+	@RequestMapping(path = "/posts/sortByDate")
+	public List<Post> sortByDate() {
+		return postService.sortByDate();
+	}
+	
+	@RequestMapping(path = "/posts/getByID")
+	public Post findPostBy_id(@RequestParam String _id){
+		return postService.findPostBy_id(_id);
+	}
+	
 }
